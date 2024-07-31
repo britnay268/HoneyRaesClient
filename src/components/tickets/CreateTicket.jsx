08172078664b4d783b5a0d1ff9fecbe7924db54d
Britnay Gore <britnaygore268@gmail.com>
2024-07-30 21:29:14 -0400

    Added layout for the form to create a ticket

--- src/components/tickets/CreateTicket.jsx
+++ src/components/tickets/CreateTicket.jsx
@@ -1,3 +1,78 @@
+import { useState } from "react";
+import { Form, FormGroup, Input, Label } from "reactstrap";
+
+// const [ticket, setTickets] = useState(null);
+// const [employee, setEmployees] = useState(null);
+// const [customer, setCustomer] = useState(null);
+
+
 export default function CreateTicket() {
-  return <h3>Submit a Ticket</h3>;
+  return (
+    <>
+      <Form>
+      <FormGroup>
+          <Label for="customer">
+            Select the Customer of the Service ticket
+          </Label>
+          <Input
+            id="customer"
+            name="customer"
+            type="select"
+          >
+            <option>
+              Yes
+            </option>
+            <option>
+              No
+            </option>
+          </Input>
+        </FormGroup>
+        <FormGroup>
+          <Label for="description">
+            Description
+          </Label>
+          <Input
+            id="exampleEmail"
+            name="email"
+            placeholder="Describe your issue"
+            type="textarea"
+          />
+        </FormGroup>
+        <FormGroup>
+          <Label for="emergency">
+            Emergency?
+          </Label>
+          <Input
+            id="emergency"
+            name="emergency"
+            type="select"
+          >
+            <option>
+              Yes
+            </option>
+            <option>
+              No
+            </option>
+          </Input>
+        </FormGroup>
+        <FormGroup>
+          <Label for="employee">
+            Pick an Employee
+          </Label>
+          <Input
+            id="emergency"
+            name="emergency"
+            type="select"
+          >
+            <option>
+              Yes
+            </option>
+            <option>
+              No
+            </option>
+          </Input>
+        </FormGroup>
+      </Form>
+    </>
+  );
 }
