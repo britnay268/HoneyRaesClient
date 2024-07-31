ddb378b181c6a2bdb9f1829712f4bd6a17e22344
Britnay Gore <britnaygore268@gmail.com>
2024-07-30 20:04:57 -0400

    Added api calls to getCustomers and getCustomer by Id

---
+++ src/data/customersData.js
@@ -0,0 +1,25 @@
+const getCustomers = () => new Promise((resolve, reject) => {
+  fetch("/api/customers", {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+  })
+  .then((resolve) => resolve.json())
+  .then((data) => resolve(data))
+  .catch(reject);
+});
+
+const getCustomer = (id) => new Promise((resolve, reject) => {
+  fetch(`/api/customers/${id}`, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+  })
+  .then((resolve) => resolve.json())
+  .then((data) => resolve(data))
+  .catch(reject);
+});
+
+export {getCustomers, getCustomer};
