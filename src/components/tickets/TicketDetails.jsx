a420cfe8666d4c028bcda66a4da9c3a09b5a5672
Britnay Gore <britnaygore268@gmail.com>
2024-08-03 12:03:57 -0400

    Added an assign button on details page for tickets without an employee

--- src/components/tickets/TicketDetails.jsx
+++ src/components/tickets/TicketDetails.jsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { Table } from "reactstrap";
 import { getServiceTicket } from "../../data/serviceTicketsData";
+import { Button } from "reactstrap";
 
 export default function TicketDetails() {
   const { id } = useParams();
@@ -18,29 +19,36 @@ export default function TicketDetails() {
   }
 
   return (
-    <Table>
-      <tbody>
-        <tr>
-          <th scope="row">Customer</th>
-          <td>{ticket.customer.name}</td>
-        </tr>
-        <tr>
-          <th scope="row">Description</th>
-          <td>{ticket.description}</td>
-        </tr>
-        <tr>
-          <th scope="row">Emergency</th>
-          <td>{ticket.emergency ? "yes" : "no"}</td>
-        </tr>
-        <tr>
-          <th scope="row">Employee</th>
-          <td>{ticket.employee?.name || "Unassigned"}</td>
-        </tr>
-        <tr>
-          <th scope="row">Completed?</th>
-          <td>{ticket.dateCompleted?.split("T")[0] || "Incomplete"}</td>
-        </tr>
-      </tbody>
-    </Table>
+    <>
+      <Table>
+        <tbody>
+          <tr>
+            <th scope="row">Customer</th>
+            <td>{ticket.customer.name}</td>
+          </tr>
+          <tr>
+            <th scope="row">Description</th>
+            <td>{ticket.description}</td>
+          </tr>
+          <tr>
+            <th scope="row">Emergency</th>
+            <td>{ticket.emergency ? "yes" : "no"}</td>
+          </tr>
+          <tr>
+            <th scope="row">Employee</th>
+            <td>{ticket.employee?.name || "Unassigned"}</td>
+          </tr>
+          <tr>
+            <th scope="row">Completed?</th>
+            <td>{ticket.dateCompleted?.split("T")[0] || "Incomplete"}</td>
+          </tr>
+        </tbody>
+      </Table>
+      {
+        ticket.employeeId == null 
+        ? <Button>Assign</Button>
+        : ''
+      }
+    </>
   );
 }
